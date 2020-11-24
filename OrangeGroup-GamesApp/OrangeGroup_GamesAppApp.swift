0951a0e31564e480a5ce65a6bc2845b43c49d5f4
anastasia.kantor <anastasia.kantor@gmail.com>
2020-11-24 10:34:45 +0100

    Initial Commit

---
+++ OrangeGroup-GamesApp/OrangeGroup_GamesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeGroup_GamesAppApp.swift
+//  OrangeGroup-GamesApp
+//
+//  Created by Anastasia Kantor on 2020-11-24.
+//
+
+import SwiftUI
+
+@main
+struct OrangeGroup_GamesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
