48753a861ff4995f0bfef20fffb3bd2492662687
Linda Levin <linda@onlinecoach.nu>
2020-12-09 18:32:42 +0100

    Ändrat färg till gradient på homeview samt vit cornerradius runt varje spelcard

--- OrangeGroup-GamesApp/HomeView.swift
+++ OrangeGroup-GamesApp/HomeView.swift
@@ -15,7 +15,11 @@ struct HomeView: View {
     
     var body: some View {
         
-        
+        ZStack {
+                    
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.purple]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
         VStack {
             VStack {
                 Image("Pojkeglad")
@@ -25,8 +29,6 @@ struct HomeView: View {
                     
             }
             .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .border(Color.white, width: 5)
-            .cornerRadius(10)
             .onTapGesture(perform: {
                 goChildMemo = true
                 print("Tryckt på barnmemo")
@@ -35,6 +37,10 @@ struct HomeView: View {
                         CardSide()
                         
                     })
+            
+            .padding()
+            .background(Color.white)
+            .cornerRadius(15)
           
             
             VStack {
@@ -44,37 +50,51 @@ struct HomeView: View {
                 Text("Pappa Memo")
             }
             .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .border(Color.black, width: 1)
-            .onTapGesture(perform: {
+                .onTapGesture(perform: {
                 goDadMemo = true
                 print("Tryckt på pappamemo")
             })
             .fullScreenCover(isPresented: $goDadMemo, content: {
                         PappaMemo()
                     })
+            
+            .padding()
+            .background(Color.white)
+            .cornerRadius(15)
                 
             VStack {
                 Image("Mammaglad")
                     .resizable()
                     .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 Text("Mamma Memo")
-            }
+                }
+
             .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .border(Color.black, width: 1)
-            .onTapGesture(perform: {
+                .onTapGesture(perform: {
                 goMomMemo = true
                 print("Tryckt på mammamemo")
             })
+            
             .fullScreenCover(isPresented: $goMomMemo, content: {
                         MammaMemo()
-                    })
+                           
+            })
+            .padding()
+            .background(Color.white)
+            .cornerRadius(15)
+            
         }.onAppear() {
             goChildMemo = false
             goDadMemo = false
             goMomMemo = false
         }
+        
+            
     }
+        
 }
+}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
